35ca66a9e854175426fe9cebc76ebadc4ba6b769
kudaeva <angelforevor@mail.ru>
2014-04-30 13:01:32 +0400

    Calculator

--- calc.js
+++ calc.js
@@ -5,13 +5,12 @@ var PendingOp = "";
 
 // обработчик нажатия 
 // цифровой кнопки
-
 function NumPressed (Num){
 		if (FlagNewNum){
 			Fcalc.ReadOut.value = Num;
 			FlagNewNum = false;
 		}else{
-			if (Fcalc.ReadOut.value == "0")
+			if ((Fcalc.ReadOut.value == "0") || (Fcalc.ReadOut.value =='Ошибка вычислений'))
 				Fcalc.ReadOut.value = Num;
 			else
 				Fcalc.ReadOut.value += Num;
@@ -22,18 +21,22 @@ function NumPressed (Num){
 // кнопки действия
 function Operation (Op){
 	var Readout = Fcalc.ReadOut.value;
+
+	if( Readout =='Ошибка вычислений'){
+		Readout = 0;
+		Fcalc.ReadOut.value = 0;
+	}
+	
 	if (FlagNewNum && PendingOp != "="){
-	Fcalc.ReadOut.value = Currents+PendingOp;
+		Fcalc.ReadOut.value = Currents+PendingOp;
 	}else{
-	FlagNewNum = true;
-	if ( '+' == PendingOp ){
-	Currents += parseFloat(Readout);
-	Fcalc.ReadOut.value+=PendingOp;
+		FlagNewNum = true;
+		if ( '+' == PendingOp ){
+		Currents += parseFloat(Readout);
+		Fcalc.ReadOut.value+=PendingOp;
 		}else if ( '-' == PendingOp ){
-			
 			Fcalc.ReadOut.value+=PendingOp;
 			Currents -= parseFloat(Readout);
-			
 		}else if ( '/' == PendingOp ){
 			Currents /= parseFloat(Readout);
 			Fcalc.ReadOut.value+=PendingOp;
@@ -42,12 +45,20 @@ function Operation (Op){
 			Fcalc.ReadOut.value+=PendingOp;
 		}else
 			Currents = parseFloat(Readout);
-		if(Op!="=") Fcalc.ReadOut.value = Currents + Op;
+		if (Op != "=") Fcalc.ReadOut.value = Currents + Op;
 		else Fcalc.ReadOut.value = Currents;
 		PendingOp = Op;
 	}
-}
 	
+	//исключение NaN и Infinity
+	if((isNaN(Currents))||(Currents==Infinity)) {
+	Currents=0;
+	Fcalc.ReadOut.value ='Ошибка вычислений';
+	return;		
+	}
+	
+	
+}
 // добавление десятичной точки с числу
 function Decimal (){
 	var curReadOut = Fcalc.ReadOut.value;
@@ -72,7 +83,6 @@ function Clear () {
 	Currents = 0;
 	PendingOp = "";
 	ClearEntry();
-
 }
 
 
@@ -81,20 +91,28 @@ function Neg (){
 	Fcalc.ReadOut.value = 
 	parseFloat(Fcalc.ReadOut.value) * -1;
 }
-//обработчик нажатия клавиши	
+
+//обработчик нажатия клавиши клавиатуры
 document.onkeypress = function pressed(event){
+	//обработка цифр
 	if ((event.keyCode >= 48)&&(event.keyCode <= 57)){
 		NumPressed( String.fromCharCode( event.keyCode));
 		return true;
 	}
+	
+	//обработка знаков операций
 	if ((event.keyCode==42)||(event.keyCode==43)||(event.keyCode==45)||(event.keyCode==47)){
-		Operation(String.fromCharCode( event.keyCode));
+		Operation( String.fromCharCode( event.keyCode));
 		return true;
 	}
+	
+	//обработка точки
 	if(event.keyCode==46){
-	Decimal();
-	return true;
+		Decimal();
+		return true;
 	}
+	
+	//обработка нажатия клавиши ENTER
 	if (event.keyCode==13){
 		Operation('=');
 		return true;
